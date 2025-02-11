d9ac6ff5bdc740f47fb72d6ea6a87aa709fcbf70
fernando-espinosa-clip <fernando.espinosa@payclip.com>
2025-02-11 01:15:27 -0600

    Update tsconfig and Vite config for path and build setup
    Refined `baseUrl` in tsconfig for consistency and added `exclude` for build directories. Introduced `__dirname` handling in Vite config to support ESM compatibility and maintain alias functionality.

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,17 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import path from 'path';
+import { fileURLToPath } from 'url';
+
+// Obtener __dirname como en CommonJS
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'), // Alias for src folder
+      '@': path.resolve(__dirname, './src'), // Alias para la carpeta src
     },
   },
 });
